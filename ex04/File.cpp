4f1f899d31c688a3930562831f3bd02093177bf3
Sumi Seo <sumseo@paul-f5Ar2s4.clusters.42paris.fr>
2024-11-13 16:34:44 +0100

    fix/ex04

--- ex04/File.cpp
+++
@@ -1,8 +0,0 @@
-#include "File.hpp"
-
-File::File(std::string name, std::string strCur,
-	std::string strRepl) : name(name), strCur(strCur), strRepl(strRepl){};
-void File::copyFile(std::string name){};
-void File::replaceString(std::string name, std::string strCur,
-	std::string strRepl){};
-File::~File(void){};
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,13 +1,53 @@
-#include "File.hpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+
+void	replaceAndWrite(std::stringstream &buffer, const std::string &src,
+		const std::string &dest, std::ostream &outfile)
+{
+	size_t	i;
+	size_t	srcLength;
+
+	i = 0;
+	std::string content = buffer.str();
+	srcLength = src.length();
+	while (i < content.length())
+	{
+		// Check if the substring matches `src`
+		if (content.substr(i, srcLength) == src)
+		{
+			outfile << dest; // Write `dest` to `outfile`
+			i += srcLength;  // Skip over `src` in the input
+		}
+		else
+		{
+			outfile << content[i]; // Write the current character to `outfile`
+			i++;                   // Move to the next character
+		}
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	if (argc != 4)
-		std::cout << "Argument number is wrong" << std::endl;
-
-	else
 	{
-		(void)argv;
-		std::cout << "hi" << std::endl;
+		std::cout << "Argument number is wrong" << std::endl;
+		return (1);
 	}
+	std::ifstream infile(argv[1]);
+	if (!infile)
+	{
+		std::cout << "The file does not exist" << std::endl;
+		return (1);
+	}
+	std::string src;
+	src = argv[2];
+	std::string dest;
+	dest = argv[3];
+	std::string new_file = (std::string(argv[1]) + ".replace").c_str();
+	std::ofstream outfile(new_file.c_str());
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	replaceAndWrite(buffer, src, dest, outfile);
 }
